b325cc5305a6a197f642dfb04cebf5064c6d3b4b
gurdeep-iqsay <gd@int.iqsay.com>
2023-04-13 14:36:34 +0530

    fixes

--- src/containers/AboutPage/style.js
+++ src/containers/AboutPage/style.js
@@ -4,6 +4,20 @@ export const AboutInner = styled.section`
         position:realtive;
         padding:60px 0;
         background: rgba(221, 221, 221, 0.17);
+        h2{
+            position:relative;
+            margin-bottom:44px;
+            color:#b6142c;
+            &:after{
+                position: absolute;
+                bottom: -22px;
+                content: "";
+                left: 0;
+                height: 4px;
+                width: 70px;
+                background: #b6142c;
+            }
+        }
         img {
             width: 100%;
             height: 100%;
--- src/containers/HomePage/style.js
+++ src/containers/HomePage/style.js
@@ -5,12 +5,24 @@ export const TextSection = styled.section`
     position:relative;
     padding: 50px 0;
     &.text-sec{
-        background: rgb(221 221 221 / 17%);
+        background: rgb(249 252 255);
         margin-top: 50px;
     }
     h2{
         font-size:30px;
         font-weight:500;
+        position: relative;
+        margin-bottom: 44px;
+        color:#b6142c;
+        &:after{
+            position: absolute;
+            bottom: -22px;
+            content: "";
+            left: 0;
+            height: 4px;
+            width: 70px;
+            background: #b6142c;
+        }
     }
     .text-img {
         width: 450px;
